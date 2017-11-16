84329c21a1e6b61b849c4a9be3875675878e0d94
aha-oretama <sekine_y_529@msn.com>
2017-11-16 16:07:19 +0900

    first commit

---
+++ src/main/java/aha/oretama/jp/springbootfasttestdemo/SpringbootFasttestDemoApplication.java
@@ -0,0 +1,12 @@
+package aha.oretama.jp.springbootfasttestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFasttestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFasttestDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/aha/oretama/jp/springbootfasttestdemo/SpringbootFasttestDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package aha.oretama.jp.springbootfasttestdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootFasttestDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
